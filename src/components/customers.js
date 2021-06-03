24cc4425a6e90ab4b780621566b7d8e9d9696888
Raj <coolrajkamal22@gmail.com>
2021-06-03 10:29:01 +0530

    toggle button and functionality

--- src/components/customers.js
+++ src/components/customers.js
@@ -1,19 +1,49 @@
-import React from 'react'
-const Customers = ({ customers, loading}) => {
-    if(loading) {
-        return <h2>Loading...</h2>
-    }
-    return (
-        <ul>
-            {
-                customers.map(customer => (
-                    <li key={customer.id}>
-                        {customer.firstname}
-                    </li>
-                ))
-            };
-        </ul>
-    )
+import React, { Fragment, useState } from "react";
+const Customers = ({ customers, loading }) => {
+    
+    const [isActive, setActive] = useState(true);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  const toggleBid = () => {
+      setActive(!isActive);
+  };
+
+  return (
+    <Fragment>
+        <button onClick={toggleBid}>{isActive ? <p>Max</p> : <p>Min</p>}</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Customer Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Premium</th>
+            <th>Max/Min Bid</th>
+          </tr>
+        </thead>
+        <tbody>
+          {customers.map((customer) => (
+            <tr key={customer.id}>
+                <td>{customer.firstname} {customer.lastname}</td>
+                <td>{customer.email}</td>
+                <td>{customer.phone}</td>
+                <td>{customer.hasPremium}</td>
+                <td>{isActive ? Math.max.apply(Math, customer.bids.map((bid) => (bid.amount))) : Math.min.apply(Math, customer.bids.map((bid) => (bid.amount)))}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {/* <ul>
+        {customers.map((customer) => (
+          <li key={customer.id}>{customer.firstname}</li>
+        ))}
+        ;
+      </ul> */}
+    </Fragment>
+  );
 };
 
 export default Customers;
